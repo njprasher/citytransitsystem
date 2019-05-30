5139918a991b500f65c25f44f6c5e90ec085a884
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-05-30 13:33:21 -0400

    [Neeraj] Added getter, setter Functions for User class.

--- src/main/java/com/transit/users/User.java
+++ src/main/java/com/transit/users/User.java
@@ -10,5 +10,34 @@ public abstract class User implements IDisplay
 
     public User(){
     }
-    
+
+    public User(int id, String password, String loginStatus) {
+        this.id = id;
+        this.password = password;
+        this.loginStatus = loginStatus;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLoginStatus() {
+        return loginStatus;
+    }
+
+    public void setLoginStatus(String loginStatus) {
+        this.loginStatus = loginStatus;
+    }
 }
