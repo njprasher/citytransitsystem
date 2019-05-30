bfe291d0c612fb4dcce27f34d33756c2f11430f1
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-05-30 13:51:58 -0400

    [Neeraj] Added Rider Class Attributes.

--- src/main/java/com/transit/users/Rider.java
+++ src/main/java/com/transit/users/Rider.java
@@ -1,4 +1,19 @@
 package com.transit.users;
 
-public class Rider {
+import com.transit.card.Card;
+
+import java.util.Date;
+
+public class Rider extends User
+{
+    private String riderName;
+    private Date riderBirth;
+    private int riderBalance;
+    private boolean riderPass;
+    private String riderEmail;
+    private int riderContact;
+    private int recentTicketId;
+    private int categoryId;
+    private Card card;
+    
 }
