b1e3cd986d2bbf195af8f088d40d2152c9f2772e
hargunmarya <hargunmarya2726@gmail.com>
2019-05-30 15:19:27 -0400

    [Hargun] Added getter and setter for class Driver

--- src/main/java/com/transit/users/Administrator.java
+++ src/main/java/com/transit/users/Administrator.java
@@ -5,6 +5,33 @@ public class Administrator extends User
     private String adminName;
     private String adminEmail;
 
-    
+    public Administrator(){
+
+    }
+    public Administrator( String adminName,String adminEmail) {
+        this.adminName = adminName;
+        this.adminEmail = adminEmail;
+    }
+    public Administrator(int id,String password,String loginStatus){
+    this.id=id;
+    this.password=password;
+    this.loginStatus=loginStatus;
+    }
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+
+    public String getAdminEmail() {
+        return adminEmail;
+    }
+
+    public void setAdminEmail(String adminEmail) {
+        this.adminEmail = adminEmail;
+    }
 }
 
