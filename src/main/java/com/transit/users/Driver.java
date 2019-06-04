7401a5295ec9e36824a344ccfd4ec795d16c759f
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-06-04 17:50:43 -0400

    [Gurwinder] Added Driver class and all the attributes and classes inside it.

--- src/main/java/com/transit/users/Driver.java
+++ src/main/java/com/transit/users/Driver.java
@@ -1,18 +1,24 @@
 package com.transit.users;
 
-import com.transit.other.Validation_Classes.ValidateUsers;
 
-public class Driver extends User{
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Driver extends User {
 
     private String driverName;
     private String driverEmail;
     private  String driverPhone;
-    private static int driverSalary;
+    private static double driverSalary;
     private String driverBirth;
-    private ValidateUsers validateUsers;
-
 
     public Driver() {
+    }
+
+    public Driver(String driverName, String driverBirth, String driverEmail, String driverPhone, String birth) {
     }
 
     /**
@@ -30,9 +36,6 @@ public class Driver extends User{
         this.driverPhone = driverPhone;
         this.driverSalary = driverSalary;
         this.driverBirth = driverBirth;
-
-
-
     }
 
 
@@ -45,38 +48,60 @@ public class Driver extends User{
     }
 
     public String getDriverEmail() {
-        validateUsers.isValidEmailAddress(this.driverEmail);
+        //   isEmailValid(this.driverEmail);
         return driverEmail;
     }
 
-    public void setDriverEmail(String driverEmail) {
-        this.driverEmail = driverEmail;
-        validateUsers.isValidEmailAddress(this.driverEmail);
 
+
+    public void setDriverEmail(String driverEmail) {
+//        isEmailValid(driverEmail);
+        this.driverEmail = driverEmail;
     }
 
     public String getDriverPhone() {
+        //   isContactNumberValid(this.driverPhone);
         return driverPhone;
     }
 
     public void setDriverPhone(String driverPhone) {
+        //    isContactNumberValid(driverPhone);
         this.driverPhone = driverPhone;
     }
 
-    public int getDriverSalary() {
-        validateUsers.validateMobileNumber(this.driverPhone);
+    public double getDriverSalary() {
         return driverSalary;
     }
 
-    public void setDriverSalary(int driverSalary) {
+    public void setDriverSalary(double driverSalary) {
         Driver.driverSalary = driverSalary;
     }
 
     public String getDriverBirth() {
+        SimpleDateFormat format = new SimpleDateFormat(this.driverBirth);
+        try
+        {
+            Date date = format.parse(this.driverBirth);
+            System.out.println(date);
+        } catch (ParseException e)
+        {
+            e.printStackTrace();
+        }
+        // formatting
+        System.out.println(format.format(new Date()));
         return driverBirth;
     }
 
     public void setDriverBirth(String driverBirth) {
+        SimpleDateFormat format = new SimpleDateFormat(driverBirth);
+        try {
+            Date date = format.parse(driverBirth);
+            System.out.println(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        // formatting
+        System.out.println(format.format(new Date()));
         this.driverBirth = driverBirth;
     }
 
@@ -86,16 +111,98 @@ public class Driver extends User{
 
     public void updateProfile(String driverName, String driverEmail, String driverPhone)
     {
+
         setDriverName(driverName);
         setDriverEmail(driverEmail);
         setDriverPhone(driverPhone);
     }
 
 
+    @Override
+    public String Display() {
+        return ("----------------------" +
+                "\n-----User Details-----" +
+                "\n----------------------" +
+                "\nName: " + this.getDriverName() +
+                "\nDate of Birth: " + this.getDriverBirth() +
+                "\nEmail: " + this.getDriverEmail() +
+                "\nContact Number: " + this.getDriverPhone()
+        );
+    }
+
+    public double yearlySalary() {
+        double yearlySalary;
+        yearlySalary = (driverSalary * 12);
+        return yearlySalary;
+    }
+
+    public void displayYearlySalary() {
+        System.out.println(driverName+","+yearlySalary());
+    }
 
 
-    /*public DriverType neeraj(){
 
-        return DriverType.Brampton;
-    }*/
+    public void inputDriverDataAndDisplay()
+    {
+
+
+        ArrayList<Driver> driverArrayList = new ArrayList<>();
+
+        Scanner scan = new Scanner(System.in);
+        boolean isRunning = true;
+
+        while(isRunning)
+        {
+            System.out.println("Driver, please enter your name(quit to exit program): ");
+            String driverName = scan.nextLine();
+            if(driverName.equalsIgnoreCase("quit"))
+
+            {
+                break;
+            }
+
+
+            System.out.println("Please enter your Email: ");
+
+            String tempDriverEmail = scan.nextLine();
+
+            if (isEmailValid(tempDriverEmail))
+
+            { driverEmail = tempDriverEmail;
+
+                System.out.println("Email entered Successfully");
+
+            } else
+            {
+                System.out.println("Incorrect Email"); //add java gotoStatement
+                driverEmail = "Incorrect Email";
+            }
+
+            System.out.println("Please Enter Your Contact Number");
+
+            String tempDriverContact = scan.nextLine();
+
+            if (isContactNumberValid(tempDriverContact))
+
+            { driverPhone = tempDriverContact;
+
+                System.out.println("Contact Number entered Successfully");
+
+            } else
+            {
+                System.out.println("Incorrect Contact Number"); //add java gotoStatement if possible
+                driverPhone = null;
+            }
+
+            Driver a = new Driver(driverName, driverBirth, driverEmail, driverPhone,driverBirth);
+            driverArrayList.add(a);
+
+
+            for(Driver t: driverArrayList) {
+                System.out.println("Name: " + t.getDriverName() + "\nEmail: " + t.getDriverEmail()+ "\nContact Number: " + t.getDriverPhone());
+                System.out.println();
+            }
+        }
+    }
+
 }
